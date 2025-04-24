85fb27db6f7002d16d592784d8c553cb1adf11de
guptarg09 <guptarg3a@gmail.com>
2025-04-24 19:05:20 +0000

    increaing and decreasing no. project

---
+++ 03-counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  const increment = () => {
+    setCounter(counter+1)
+  } 
+
+  const decrement = () => {
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+      <h1>ritesh</h1>
+      <h2>Counter value: {counter}</h2> 
+
+      <button onClick={increment}>Increse</button>  {/* here we provide refrence of function not 
+      increment() because we want to execute the function when button is pressed*/}
+      <button onClick={decrement}>Decrease</button>
+    </>
+  )
+}
+
+export default App
